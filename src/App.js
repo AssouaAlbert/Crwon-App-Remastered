b43f0b477a769f6b951948ad4d6cbd94d6c9db40
Albert Assoua <assouaalbert@gmail.com>
2023-02-01 00:18:26 -0500

    Use the useRoute hook to create and manage routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useRoutes } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from "./components/navigation/navigation.component";
 import Shop from './routes/shop/shop.component';
+import React from 'react';
 
 const App = () => {
+  const endpoints = useRoutes([
+    {
+      path: "/",
+      element: <Navigation />,
+      children: [
+        {
+          index: true,
+          element: < Home />,
+        },
+        {
+          path:"shop",
+          index: true,
+          element: < Shop />,
+        }
+      ]
+    },
 
+  ])
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
-      </Route>
-    </Routes>
+    <React.Fragment>
+    {endpoints}
+    </React.Fragment>
+    // <Routes>
+    //   <Route path="/" element={<Navigation />}>
+    //     <Route index element={<Home />} />
+    //     <Route path="shop" element={<Shop />} />
+    //   </Route>
+    // </Routes>
   );
 }
 
